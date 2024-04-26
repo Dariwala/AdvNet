c2df097d853ef2829d3aa8516b1d1023c24161da
Shehab Sarar Ahmed <daeeshehaba@gmail.com>
2024-04-26 16:32:57 -0500

    Some tweaks

--- single_cc/problem.py
+++ single_cc/problem.py
@@ -2,11 +2,6 @@ from pymoo.core.problem import Problem, ElementwiseProblem
 import numpy as np
 from single_cc.evaluate import evaluate
 
-def preprocess(x):
-    for i, y in enumerate(x):
-        x[i] = round(y)
-    return x
-
 class SingleCCProblem(ElementwiseProblem):
     def __init__(self, trace_length, lower_bound, upper_bound, ref, n_evals, **kwargs):
         self.trace_length = trace_length
@@ -18,6 +13,5 @@ class SingleCCProblem(ElementwiseProblem):
         super().__init__(n_var=trace_length, n_obj=1, n_ieq_constr=0, n_eq_constr = 0, xl=lower_bound, xu=upper_bound, **kwargs)
 
     def _evaluate(self, x, out, *args, **kwargs):
-        x = preprocess(list(x))
-        score = evaluate(x, self.ref, self.n_evals)
+        score = evaluate(list(x), self.ref, self.n_evals)
         out["F"] = - score
