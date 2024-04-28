a13f987a70fdfcc5c10b4ccb6c27007e3fe71bab
Shehab Sarar Ahmed <daeeshehaba@gmail.com>
2024-04-27 22:50:33 -0500

    Evaluating a single trace

--- analyze_single_cc_trace.py
+++ analyze_single_cc_trace.py
@@ -1,11 +1,13 @@
 import argparse
 from single_cc.evaluate import evaluate
+import os
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--trace', nargs='+', type=int, default=[4000,20,3000], help='Trace to evaluate when alg=2')
+    parser.add_argument('--trace', nargs='+', type=int, default=[4000,20,3000], help='Trace to evaluate')
     parser.add_argument('--ref', type=str, default="cubic", help='Reference algorithm')
     args = parser.parse_args()
-
+    os.system("iperf -s &")
     score = evaluate(args.trace, args.ref, 1, True)
     print(score)
+    os.system("pkill -9 iperf")
