32bf6d66167f112e29dcb24fd7b3156a486f7cea
Shehab Sarar Ahmed <daeeshehaba@gmail.com>
2024-05-15 16:14:43 -0500

    Simplification script without testing

--- Simplify/single_cc_simplify.py
+++ Simplify/single_cc_simplify.py
@@ -1,5 +1,6 @@
 import numpy as np
 from single_cc.evaluate import evaluate
+import copy
 
 class SingleCCSimplify():
     def __init__(self, initial_trace, pc, mpd, ref):
@@ -32,9 +33,9 @@ class SingleCCSimplify():
             sum += np.abs(trace[k] - trace[k-1])
         return sum
     
-    def slice(self, timesteps):
-        initial_timesteps = len(self.initial_trace) // 3
-        return self.initial_trace[:timesteps] + self.initial_trace[initial_timesteps: initial_timesteps + timesteps] + self.initial_trace[2 * initial_timesteps: 2 * initial_timesteps + timesteps]
+    def slice(self, timesteps, trace):
+        initial_timesteps = len(trace) // 3
+        return trace[:timesteps] + trace[initial_timesteps: initial_timesteps + timesteps] + trace[2 * initial_timesteps: 2 * initial_timesteps + timesteps]
     
     def check_validity(self, c_score, p_score):
         if p_score / self.initial_score < 1 - self.mpd:
@@ -44,12 +45,42 @@ class SingleCCSimplify():
         else:
             return True
     
-    def shorten_length(self):
-        for candidate_length in range(2, len(self.initial_trace) + 1):
-            candidate_trace = self.slice(candidate_length)
+    def shorten_length(self, trace):
+        for candidate_length in range(2, len(trace) + 1):
+            candidate_trace = self.slice(candidate_length, trace)
             complexity_score = self.compute_score(candidate_trace)
             performance_score = evaluate(candidate_trace, self.ref, 3)
 
             if self.check_validity(complexity_score, performance_score):
                 return candidate_trace
     
+    def reduce_variance(self, short_trace):
+        for i in range(1, len(short_trace)):
+            if i % (len(short_trace) // 3) == 0:
+                continue
+            pct_change = (short_trace[i] - short_trace[i-1]) / short_trace[i-1]
+            sign = 1
+            if pct_change < 0:
+                sign = -1
+            for pct in range(0, int(pct_change * 100), sign * 5):
+                old = short_trace[i]
+                short_trace[i] = short_trace[i-1] * (1 + pct / 100)
+                p_score = evaluate(short_trace, self.ref, 3)
+                c_score = self.compute_score(short_trace)
+
+                if self.check_validity(c_score, p_score) == False:
+                    short_trace[i] = old
+                else:
+                    break
+    def simplify(self):
+        prev_length = len(self.initial_trace)
+        shortened_trace = copy.deepcopy(self.initial_trace)
+        while True:
+            shortened_trace = self.shorten_length(shortened_trace)
+            if prev_length == len(shortened_trace):
+                break
+            else:
+                prev_length = len(shortened_trace)
+            self.reduce_variance(shortened_trace)
+        return shortened_trace
+        
---
+++ simplify.py
@@ -0,0 +1,15 @@
+import argparse
+from Simplify.single_cc_simplify import SingleCCSimplify
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--trace', nargs='+', type=int, default=[4000,20,3000], help='Trace to simplify')
+    parser.add_argument('--ref', type=str, default="cubic", help='Reference algorithm')
+    parser.add_argument('--mpd', type=float, default=0.1, help='Maximum relative performance decrease')
+    parser.add_argument('--pc', type=float, default=0.1, help='Minimum performance to complexity ratio')
+    args = parser.parse_args()
+
+    Simplify = SingleCCSimplify(args.trace, args.pc, args.mpd, args.ref)
+
+    simplified_trace = SingleCCSimplify.simplify()
+    
