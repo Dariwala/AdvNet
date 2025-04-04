da508b0967a3fcef25b9739a6d594009cc1c2221
Shehab Sarar Ahmed <daeeshehab@gmail.com>
2025-04-03 20:04:56 -0500

    iperf changed to iperf3

--- dchannel/convert.py
+++ dchannel/convert.py
@@ -7,7 +7,7 @@ def convert(trace):
     scale_up(bandwidths_2, 500)#l=2,u=10
     scale_up(latencies, 5)#l=1,u=25
     scale_up(durations, 5)#l=1,u=20
-    data *= 50 #l=1,u=10
+    data *= 500 #l=1,u=10
     queue_length *= 100 #l=1,u=50
 
     return bandwidths_1 + bandwidths_2 + latencies + durations + [data, ll_latency, queue_length]
--- dchannel/evaluate.py
+++ dchannel/evaluate.py
@@ -18,12 +18,12 @@ def create_commands(bw_file_1, bw_file_2, lt_file, ll_latency, data_size, heuris
         command1 = "mm-adv-delay-link-rrc 15 "+heuristic+" --enable-buffer-uplink --enable-buffer-downlink --constant-x=0.75 "+parent_folder+"AdvNet/traces/"+lt_file+" "+str(ll_latency)+" "+parent_folder+"packet-logs"+" "+parent_folder+"AdvNet/traces/"+bw_file_1+" "+parent_folder+"AdvNet/traces/"+bw_file_1+" "+parent_folder+"AdvNet/traces/"+bw_file_2+" "+parent_folder+"AdvNet/traces/"+bw_file_2+" --uplink-queue=droptail --uplink-queue-args=packets="+str(queue_length)
         # print(command1)
     command2 = "sleep 1"
-    command3 = "/usr/bin/time -f \"%e\" iperf -c 100.64.0.1 -n "+str(data_size)+"K"
+    command3 = "/usr/bin/time -f \"%e\" iperf3 -c 100.64.0.1 -i 0.5 -n "+str(data_size)+"K"
     command4 = "exit"
     command5 = "pkill -9 iperf"
 
-    os.system("pkill -9 iperf")
-    os.system("sleep 0.1;iperf -s &")
+    os.system("sudo pkill -9 iperf")
+    os.system("sleep 0.1;iperf3 -s &")
 
     print(command1)
     print(command2)
@@ -36,13 +36,35 @@ def create_commands(bw_file_1, bw_file_2, lt_file, ll_latency, data_size, heuris
         """
     return commands
 
+def parse(output):
+    output = output.split('\n')
+
+    for i in range(len(output)):
+        if output[i][:3] == "- -":
+            start = i-1
+            logs = []
+            while True:
+                try:
+                    end_time = float(output[start].split()[2].split('-')[-1])
+                    # print(output[start].split())
+                    bitrate = float(output[start].split()[6])
+                    logs.append([end_time, bitrate])
+                except ValueError:
+                    break
+                start -= 1
+            print(logs)
+            return float(output[i-1].split()[2].split('-')[-1])
+    
+
 def measure_time(bw_file_1, bw_file_2, lt_file, ll_latency, data_size, queue_length, heuristic):
     shell = subprocess.Popen("/bin/bash", stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     commands = create_commands(bw_file_1, bw_file_2, lt_file, ll_latency, data_size, heuristic, queue_length)
     output, errors = shell.communicate(commands)
-    return float(errors)
+    return parse(output)
+    # return float(errors)
 
 def evaluate(trace, exp_type, n_evals = 1, log = False):
+    trace = trace[:-3] + [1, 1] + trace[-3:]
     if len(trace) == 7: #handling single timestep
         trace = copy.deepcopy(trace)
         trace = [trace[0], trace[0], trace[1], trace[1], trace[2], trace[2], trace[3], trace[3]] + trace[4:]
@@ -56,17 +78,27 @@ def evaluate(trace, exp_type, n_evals = 1, log = False):
 
     print(bw_file_1, bw_file_2, lt_file)
 
+    scores = []
+    logs = []
     if exp_type == 1:
-        exec_time_dc = measure_time(bw_file_1, bw_file_2, lt_file, ll_latency, data, queue_length, "pkt-state-rewards")
-        os.system("cp "+parent_folder+"packet-logs/packet-log-uplink "+parent_folder+"/packet-logs/tar_packet-log-uplink")
-        os.system("cp "+parent_folder+"packet-logs/packet-log-output-uplink "+parent_folder+"/packet-logs/tar_packet-log-output-uplink")
-        os.system("cp "+parent_folder+"packet-logs/packet-log-downlink "+parent_folder+"/packet-logs/tar_packet-log-downlink")
-        os.system("cp "+parent_folder+"packet-logs/packet-log-output-downlink "+parent_folder+"/packet-logs/tar_packet-log-output-downlink")
-        exec_time_hb = measure_time(bw_file_1, bw_file_2, lt_file, ll_latency, data, queue_length, "all-hb")
+        for _ in range(n_evals):
+            exec_time_dc = measure_time(bw_file_1, bw_file_2, lt_file, ll_latency, data, queue_length, "pkt-state-rewards")
+            os.system("cp "+parent_folder+"packet-logs/packet-log-uplink "+parent_folder+"/packet-logs/tar_packet-log-uplink")
+            os.system("cp "+parent_folder+"packet-logs/packet-log-output-uplink "+parent_folder+"/packet-logs/tar_packet-log-output-uplink")
+            os.system("cp "+parent_folder+"packet-logs/packet-log-downlink "+parent_folder+"/packet-logs/tar_packet-log-downlink")
+            os.system("cp "+parent_folder+"packet-logs/packet-log-output-downlink "+parent_folder+"/packet-logs/tar_packet-log-output-downlink")
+            exec_time_hb = measure_time(bw_file_1, bw_file_2, lt_file, ll_latency, data, queue_length, "all-hb")
+            scores.append((exec_time_dc - exec_time_hb) / exec_time_dc)
+            logs.append((exec_time_dc, exec_time_hb, scores[-1]))
         if log:
             print(bandwidths_1, bandwidths_2, latencies, durations, ll_latency, queue_length, data)
-            print(exec_time_dc, exec_time_hb)
-        return (exec_time_dc - exec_time_hb) / exec_time_dc
+            print(logs)
+        else:
+            if np.var(scores) > 0.01:
+                return -100
+            else:
+                print("ekhane", np.var(scores), logs)
+                return np.mean(scores)
     elif exp_type == 2:
         exec_time_ll = measure_time(bw_file_1, bw_file_2, lt_file, ll_latency, data, queue_length, "all-ll")
         exec_time_dc = measure_time(bw_file_1, bw_file_2, lt_file, ll_latency, data, queue_length, "pkt-state-rewards")
