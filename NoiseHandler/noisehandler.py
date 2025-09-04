5659a3ef38803233bd113d4f358df5625305c3ca
Shehab Sarar Ahmed <daeeshehab@gmail.com>
2025-09-03 22:15:22 -0500

    Added lcb method

---
+++ NoiseHandler/noisehandler.py
@@ -0,0 +1,18 @@
+import statistics
+import numpy as np
+
+class NoiseHandler:
+    def mean(self, values):
+        return sum(values) / len(values)
+    def median(self, values):
+        return statistics.median(values)
+    def trimmed_mean(self, values, pct=20):
+        trim_len = len(values) * pct // 100
+        trimmed_values = values[trim_len:-trim_len]
+        return sum(trimmed_values) / len(trimmed_values) 
+    def lcb(self, values, lam=2):
+        samples = np.array(values, dtype=float)
+        mean = np.mean(samples)
+        std = np.std(samples, ddof=1)  # sample standard deviation
+        n = len(samples)
+        return mean - lam * (std / np.sqrt(n))
